2f778e8e12bc35570310462c72e2236042f65065
Gtindi <tindigabriel@gmail.com>
2025-06-06 21:18:29 +0300

    Build run

--- funguka/src/pages/About.js
+++ funguka/src/pages/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
-
 import Header from "../components/About-header";
 import Intro from "../components/About-intro";
 import Count from "../components/Count";
